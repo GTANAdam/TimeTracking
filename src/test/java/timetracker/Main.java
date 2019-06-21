5e653996531d982fff7a81056c2bc268d5007cb8
Adam <adambh@outlook.com>
2019-06-21 03:37:05 +0300

    Fix test runner

--- src/test/java/timetracker/Main.java
+++ src/test/java/timetracker/Main.java
@@ -1,10 +1,10 @@
-package com.timetracking;
+package timetracker;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class LogoutServletTest {
+public class Main {
     @Test
     public void test() {
         assertTrue(true);
